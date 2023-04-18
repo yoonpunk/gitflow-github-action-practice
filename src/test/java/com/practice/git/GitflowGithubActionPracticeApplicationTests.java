02df7913d0ab3b9195db9c3f7719ceabd20d59ea
yoonpunk <yoonys427@outlook.com>
2023-04-19 01:12:47 +0900

    initial commit

---
+++ src/test/java/com/practice/git/GitflowGithubActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowGithubActionPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
