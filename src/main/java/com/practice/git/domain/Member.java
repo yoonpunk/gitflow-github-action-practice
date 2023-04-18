eaa41aaaa25bb3f88b1a9533898073b1d3081c21
yoonpunk <yoonys427@outlook.com>
2023-04-19 01:20:40 +0900

    Create Member entity.

---
+++ src/main/java/com/practice/git/domain/Member.java
@@ -0,0 +1,35 @@
+package com.practice.git.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @Column(name = "MEMBER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    protected Member() {
+
+    }
+
+    private Member(String name, String phone, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.name = name;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    static Member createMember(String name, String phone, LocalDateTime createdAt) {
+        return new Member(name, phone, createdAt, createdAt);
+    }
+}
