9affc43043ea6d345fc20d2106ce8fe82725a686
yoonpunk <yoonys427@outlook.com>
2023-05-01 16:08:48 +0900

    changed GitflowGithubActionPracticeApplicationTests.java to GithubActionPracticeApplicationTests.java

--- src/test/java/com/practice/git/GithubActionPracticeApplicationTests.java
+++ src/test/java/com/practice/git/GithubActionPracticeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GitflowGithubActionPracticeApplicationTests {
+class GithubActionPracticeApplicationTests {
 
     @Test
     void contextLoads() {
