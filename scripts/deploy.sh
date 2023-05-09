1d5721a40454d12a274fb5488bbb230b124a9121
yoonpunk <yoonys427@outlook.com>
2023-05-10 00:12:31 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,14 +26,15 @@ else
   kill -9 $CURRENT_PID
 fi
 
+
+export PATH=$PATH:$(which java}
+
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> deploy new jar $DEPLOY_JAR" >> $DEPLOY_LOG_PATH
 echo ">java home $(which java)" >> $DEPLOY_LOG_PATH
-echo ">java version $(/usr/bin/java -version)" >> $DEPLOY_LOG_PATH
+echo ">java version $(java -version)" >> $DEPLOY_LOG_PATH
 
-nohup $(which java) -jar -Dspring.profiles.active=prod $DEPLOY_JAR -DDB_IP=${DB_IP} -DDB_PORT=${DB_PORT} -DDB_NAME=${DB_NAME} -DDB_USER=${DB_USER} -DDB_PW=${DB_PORT}  &
-
-nohup java -jar $DEPLOY_JAR &
+nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR -DDB_IP=${DB_IP} -DDB_PORT=${DB_PORT} -DDB_NAME=${DB_NAME} -DDB_USER=${DB_USER} -DDB_PW=${DB_PORT}  &
 
 sleep 3
 
