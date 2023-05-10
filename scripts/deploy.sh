23d1a4f931635769bbf0734de4866d88fc017c46
yoonpunk <yoonys427@outlook.com>
2023-05-10 20:42:18 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -39,9 +39,9 @@ echo ">DB_IP ${DB_IP}" >> $DEPLOY_LOG_PATH
 echo ">DB_PORT ${DB_PORT}" >> $DEPLOY_LOG_PATH
 echo ">DB_NAME ${DB_NAME}" >> $DEPLOY_LOG_PATH
 echo ">DB_USER ${DB_USER}" >> $DEPLOY_LOG_PATH
-echo ">DB_PORT ${DB_PORT}" >> $DEPLOY_LOG_PATH
+echo ">DB_PW ${DB_PW}" >> $DEPLOY_LOG_PATH
 
-nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR -DDB_IP=${DB_IP} -DDB_PORT=${DB_PORT} -DDB_NAME=${DB_NAME} -DDB_USER=${DB_USER} -DDB_PW=${DB_PORT}  &
+nohup java -jar -Dspring.profiles.active=prod $DEPLOY_JAR -DDB_IP=${DB_IP} -DDB_PORT=${DB_PORT} -DDB_NAME=${DB_NAME} -DDB_USER=${DB_USER} -DDB_PW=${DB_PW}  &
 
 sleep 3
 
